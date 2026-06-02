0dfe7412df63076cedff61facb1dcbe6cb5299f6
Nirmala2599 <nirmalat931@gmail.com>
2026-06-02 19:54:24 +0530

    mail

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -76,7 +76,9 @@ router.post("/forgot-password", async (req, res) => {
 console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
